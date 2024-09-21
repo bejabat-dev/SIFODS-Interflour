e1b6ba839bc5f919b22e412e13b46cba2c763362
bejabat-dev <bejabattailor@gmail.com>
2024-09-21 18:08:54 +0700

    1.0

--- lib/pages/log_details.dart
+++ lib/pages/log_details.dart
@@ -6,22 +6,102 @@ class LogDetails extends StatelessWidget {
   const LogDetails({super.key, required this.addTruck});
   final AddTruck addTruck;
 
+  final TextStyle ts = const TextStyle(fontSize: 16);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Styles().customAppBar(context, 'Details'),
-      body: Column(crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(addTruck.nama!),
-          Text(addTruck.nopol!),
-          Text(addTruck.supir!),
-          Text(addTruck.status!),
-          Text(addTruck.telp!),
-          Text(addTruck.jumlah!.toString()),
-          Text(addTruck.volume!),
-          Text(addTruck.jenis!),
-          Text(addTruck.ekspedisi!)
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Name',
+                    style: ts,
+                  ),
+                  Text(
+                    'Licence plate number',
+                    style: ts,
+                  ),
+                  Text(
+                    'Driver',
+                    style: ts,
+                  ),
+                  Text(
+                    'Status',
+                    style: ts,
+                  ),
+                  Text(
+                    'Phone number',
+                    style: ts,
+                  ),
+                  Text(
+                    'Number of vehicles'.toString(),
+                    style: ts,
+                  ),
+                  Text(
+                    'Volume',
+                    style: ts,
+                  ),
+                  Text(
+                    'Type',
+                    style: ts,
+                  ),
+                  Text(
+                    'Expedition',
+                    style: ts,
+                  )
+                ],
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  addTruck.nama!,
+                  style: ts,
+                ),
+                Text(
+                  addTruck.nopol!,
+                  style: ts,
+                ),
+                Text(
+                  addTruck.supir!,
+                  style: ts,
+                ),
+                Text(
+                  addTruck.status!,
+                  style: ts,
+                ),
+                Text(
+                  addTruck.telp!,
+                  style: ts,
+                ),
+                Text(
+                  addTruck.jumlah!.toString(),
+                  style: ts,
+                ),
+                Text(
+                  addTruck.volume!,
+                  style: ts,
+                ),
+                Text(
+                  addTruck.jenis!,
+                  style: ts,
+                ),
+                Text(
+                  addTruck.ekspedisi!,
+                  style: ts,
+                )
+              ],
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
--- lib/widgets/log_vehicles.dart
+++ lib/widgets/log_vehicles.dart
@@ -14,58 +14,64 @@ class LogVehicles extends ConsumerWidget {
     final AsyncValue<List<dynamic>> trucks = ref.watch(getVehicleLogsProvider);
 
     return Scaffold(
-        body: switch (trucks) {
-      AsyncValue<List<AddTruck>>(:final valueOrNull?) => valueOrNull.isNotEmpty
-          ? ListView.builder(
-              itemCount: valueOrNull.length,
-              itemBuilder: (context, i) {
-                var data = valueOrNull[i];
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 1),
-                  child: Material(
-                    elevation: 1,
-                    color: Colors.white,
-                    child: InkWell(
-                      onTap: () {
-                        Tools().Navigate(context, LogDetails(addTruck: data));
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 8, horizontal: 16),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                    child: Text(
-                                  data.nama!,
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.bold),
-                                )),
-                                Text(
-                                    Tools().formatDate(data.created.toString()))
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Expanded(child: Text(data.nopol!)),
-                                Text(data.status!)
-                              ],
-                            ),
-                            Text(data.jenis!)
-                          ],
+      body: switch (trucks) {
+        
+        AsyncValue(:final error?) => Text('Error: $error'),
+        AsyncValue<List<AddTruck>>(:final valueOrNull?) => 
+            ListView.builder(
+                itemCount: valueOrNull.length,
+                itemBuilder: (context, i) {
+                  var data = valueOrNull[i];
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 1),
+                    child: Material(
+                      elevation: 1,
+                      color: Colors.white,
+                      child: InkWell(
+                        onTap: () {
+                          Tools().Navigate(context, LogDetails(addTruck: data));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8, horizontal: 16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Expanded(
+                                      child: Text(
+                                    data.nama!,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold),
+                                  )),
+                                  Text(Tools()
+                                      .formatDate(data.created.toString()))
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(child: Text(data.nopol!)),
+                                  Text(data.status!)
+                                ],
+                              ),
+                              Text(data.jenis!)
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              })
-          : const Center(child: Text('No data')),
-      AsyncValue(:final error?) => Text('Error: $error'),
-      _ => const Center(
-          child: CupertinoActivityIndicator(),
-        ),
-    });
+                  );
+                })
+           ,
+        _ => const Center(
+            child: CupertinoActivityIndicator(),
+          ),
+          
+      },
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        ref.refresh(getVehicleLogsProvider);
+      }),
+    );
   }
 }
